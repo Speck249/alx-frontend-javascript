9a14ed4bd47fff3707ffc2fbd78838ef97703d52
Rediet <redietkfle@gmail.com>
2023-09-26 13:49:52 +0000

    Return promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,10 +2,10 @@ export default function getResponseFromAPI() {
   const value = true;
   return new Promise((resolve, reject) => {
     if (value) {
-      resolve(value);
+      resolve();
     }
     else {
-      reject(Error('Operation Broke!'));
+      reject();
     }
   });
 }
