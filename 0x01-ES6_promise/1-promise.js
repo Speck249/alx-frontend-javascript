942686d0a25a20e3a9125684256373159c121f3f
Rediet <redietkfle@gmail.com>
2023-09-26 13:36:51 +0000

    Return promise with param boolean

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const obj = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if(success === true) {
+      resolve(obj);
+    }
+    else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
