99f26e4287d26de530e36dba37ce26d56625be7e
Rediet <redietkfle@gmail.com>
2023-10-01 01:48:13 +0000

    Implement getter and setter for Class constructor attributes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,22 +2,19 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
-    }
-    else {
+    } else {
       this._name = name;
     }
 
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
-    }
-    else {
+    } else {
       this._length = length;
     }
-   
+
     if (!Array.isArray(students) || students.some((element) => typeof element !== 'string')) {
       throw new TypeError('Students must be an array of strings');
-    }
-    else {
+    } else {
       this._students = students;
     }
   }
