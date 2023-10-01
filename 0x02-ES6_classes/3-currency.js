d1e40e8216d62a4b54ad207818a3902a08485091
Rediet <redietkfle@gmail.com>
2023-10-01 01:50:55 +0000

    Implement Class method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this.code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return (`${this._name} (${this._code})`);
+  }
+}
