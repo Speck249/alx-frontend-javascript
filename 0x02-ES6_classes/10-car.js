87832dd97e33bdaf973eb6b6645c8bcc9ab73471
Rediet <redietkfle@gmail.com>
2023-10-02 08:47:38 +0000

    Return new object of Car class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,41 +1,18 @@
+const cloneSymbol = Symbol('clone');
+
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new Error('Brand must be string');
-    } else {
-      this._brand = brand;
-    }
-    if (typeof motor !== 'string') {
-      throw new Error('Motor must be string');
-    } else {
-      this._motor = motor;
-    }
-    if (typeof color !== 'string') {
-      throw new Error('Color must be string');
-    } else {
-      this._color = color;
-    }
-  }
-
-  get brand() {
-    return this._brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  cloneCar() {
-    const { brand, motor, color } = this;
-    const CarClass = this.constructor[Symbol.species];
-    return new CarClass(brand, motor, color);
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   static get [Symbol.species]() {
     return this;
+  }
+
+  cloneCar() {
+    const newCar = this.constructor[Symbol.species];
+    return new newCar;
   }
 }
