c11e3dded27759d7dcefe76183e3d9155e4e645e
Rediet <redietkfle@gmail.com>
2023-10-03 10:32:44 +0000

    Return student Ids

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,8 +2,7 @@ export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  else {
-    const result = students.map((item) => item.id);
-    return result;
-  }
+
+  const result = students.map((item) => item.id);
+  return result;
 }
