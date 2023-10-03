e6fbf20d06ba7caa0e7392ad814164d052a15045
Rediet <redietkfle@gmail.com>
2023-10-03 10:20:22 +0000

    Return students list by location

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(students, city) {
-  const result = students.filter((item) => item.location.includes(city));
+  const result = students.filter((item) => item.location === city);
   return result;
 }
