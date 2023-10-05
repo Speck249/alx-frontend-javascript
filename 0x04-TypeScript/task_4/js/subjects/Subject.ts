dcb0d8437afe3fac7e99bf756cf6512d6d904092
Rediet <redietkfle@gmail.com>
2023-10-05 12:05:30 +0000

    Create class and setter method

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,14 @@
+import { Teacher } from './Teacher';
+
+namespace Subjects {
+  export class Subject {
+    private teacher: Teacher;
+    constructor(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+    
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    }
+  }
+}
