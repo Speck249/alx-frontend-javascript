0221bc4d155732988ed1bc0e6b0278709631ffcd
Rediet <redietkfle@gmail.com>
2024-09-17 00:16:56 -0700

    Update Instantiation of class

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const obj1 = new ClassRoom(19);
-  const obj2 = new ClassRoom(20);
-  const obj3 = new ClassRoom(34);
-
-  return [obj1, obj2, obj3];
+  const arrayOfInstances = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
+  return arrayOfInstances;
 }
